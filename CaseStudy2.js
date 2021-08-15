f454400a27b133417ce43d96542c2976ab7cf96b
masmisz <44495771+masmisz@users.noreply.github.com>
2021-08-16 06:09:25 +0700

    Update CaseStudy2.js

--- CaseStudy2.js
+++ CaseStudy2.js
@@ -1,12 +1,26 @@
-function nilaiPeserta (nilai) {
+function nilaiPeserta(nilai) {
     /* 
         Buat proses seleksi kondisi untuk melakukan klasifikasi nilai peserta dengan rincian:
         A = 80 - 100
         B = 60 - 80
         C = 40 - 60
         D = 20 - 40
-        E = >20
+        E = 0 - 20
     */
+
+    if (nilai > 80 && nilai <= 100) {
+        return document.write('A' + '<br>');
+    } else if (nilai > 60 && nilai <= 80) {
+        return document.write('B' + '<br>');
+    } else if (nilai > 40 && nilai <= 60) {
+        return document.write('C' + '<br>');
+    } else if (nilai > 20 && nilai <= 40) {
+        return document.write('D' + '<br>');
+    } else if (nilai >= 0 && nilai <= 20) {
+        return document.write('E' + '<br>');
+    } else {
+        return document.write('Nilai tidak valid')
+    }
 }
 
 /*
@@ -17,3 +31,9 @@ function nilaiPeserta (nilai) {
     4. 50
     5. 101
 */
+
+nilaiPeserta(5);
+nilaiPeserta(60.5);
+nilaiPeserta(39.5);
+nilaiPeserta(50);
+nilaiPeserta(101);
